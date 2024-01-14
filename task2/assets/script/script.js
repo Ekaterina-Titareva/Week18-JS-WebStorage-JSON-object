d3a002bcf85939d35b0aad8ae4b0846a2125e3d7
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-01-14 18:35:53 +0300

    added the end  of the second task

--- task2/assets/script/script.js
+++ task2/assets/script/script.js
@@ -1,48 +1,73 @@
 const taskList = document.getElementById('taskList');
 const textOfNewElement = document.getElementById('inputBox');
 const newElement = document.createElement("li");
+const noTasks = document.querySelector(".noTasks");
+const clear = document.querySelector('.clear');
+
+let jsonTaskList = localStorage.getItem('taskList');
+jsonTaskList = JSON.parse(jsonTaskList);
+
+if (jsonTaskList && jsonTaskList.length > 0) {
+//если задачи есть в локальной памяти, то они создаются после обновления страницы
+	jsonTaskList.forEach(element => {
+		noTasks.style.display = "none";
+		taskList.innerHTML += `
+			<li class="taskList__item">${element}</li>`
+		}
+	);
+	//кнопка очистить разблокирована
+	clear.disabled = false;
+} else {
+	noTasks.style.display = "inherit";
+	clear.disabled = true;
+};
+
 
 function createTask() {
 	if (textOfNewElement.value.trim() !== '') {
-	const noTasks = document.querySelector(".noTasks");
-	noTasks.style.display = "none";
-	newElement.textContent = textOfNewElement.value;
-	taskList.innerHTML += `
-	<li class="taskList__item">${textOfNewElement.value}</li>`
-
-let jsonTaskList = localStorage.getItem('taskList');
-	jsonTaskList = jsonTaskList ? JSON.parse(jsonTaskList) : [];
-
-	jsonTaskList.push(textOfNewElement.value);
-
-	localStorage.setItem('taskList', JSON.stringify(jsonTaskList));
-
-	console.log(`The task "${textOfNewElement.value}" added to todo-list and saved in the Local Storage.`);
-
-	textOfNewElement.value = '';
-} else {
-	alert('Add a task');
-}
-}
+		const createNoTasks = document.querySelector(".noTasks");
+		createNoTasks.style.display = "none";
+		//берем значение из инпута и создаем задачу
+		newElement.textContent = textOfNewElement.value;
+		taskList.innerHTML += `
+		<li class="taskList__item">${textOfNewElement.value}</li>`
+		//сохраняем задачу в локальную память
+		let jsonTaskList = localStorage.getItem('taskList');
+		jsonTaskList = jsonTaskList ? JSON.parse(jsonTaskList) : [];
+		jsonTaskList.push(textOfNewElement.value);
+		localStorage.setItem('taskList', JSON.stringify(jsonTaskList));
+		console.log(`The task "${textOfNewElement.value}" added to to do list and saved in the Local Storage.`);
+		//очищаем поле ввода
+		textOfNewElement.value = '';
+		//кнопка очистить разблокирована
+		clear.disabled = false;
+	} else {
+		alert('Add a task');
+	}
+};
 
 document.querySelector('.add').onclick = createTask;
 
 
 taskList.addEventListener('click', function(evt) {
-	if (evt.target.tagName === 'LI') {
 	// Код, который будет выполнен при клике на элемент <li>
-	evt.target.classList.toggle('completed'); // переключение класса
-}
+	if (evt.target.tagName === 'LI') {
+	// переключение класса
+	evt.target.classList.toggle('completed');
+	}
 });
 
 const clearList = () => {
 	let jsonTaskList = localStorage.getItem('taskList');
 	jsonTaskList = JSON.parse(jsonTaskList);
+	//очищаем список задач и локальную память
 	jsonTaskList =[];
 	localStorage.setItem('taskList', JSON.stringify(jsonTaskList));
+	//добавляем надпись, что задачи отсутствуют
 	taskList.innerHTML = `<p class="noTasks">There are no tasks</p>`;
 	console.log('The To do list is cleared');
-
+	//кнопка очистить заблокирована
+	clear.disabled = true;
 };
 
 document.querySelector('.clear').addEventListener('click', clearList);
